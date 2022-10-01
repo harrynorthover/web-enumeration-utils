044310970f1b814367d9b358114c65fdfdf7af16
Harry Northover <harry@nrthpoint.com>
2022-10-01 17:35:38 +0100

    Latest

--- generate-safe-wordlist.py
+++ generate-safe-wordlist.py
@@ -11,34 +11,76 @@ parser.add_argument(
     "--validPassword", help="A valid username", type=str)
 
 parser.add_argument(
+    "--user", help="Username wordlist", type=str)
+
+parser.add_argument(
     "--users", help="Username wordlist", type=str)
 
 parser.add_argument(
-    "--password", help="Password wordlisr", type=str)
+    "--password", help="Password wordlist", type=str)
+
+parser.add_argument(
+    "--passwords", help="Password wordlist", type=str)
 
 args = parser.parse_args()
 
-correctLogin = ""
-correctPassword = ""
 usernames = ""
 passwords = ""
 
-try:
-    usWlFile: TextIOWrapper = open(args.users, "r")
-except FileNotFoundError:
-    error("Wordlist does not exist!")
-else:
-    values = usWlFile.read()
-    usernames = values.split('\n')
-    usWlFile.close()
+newUsers = []
+newPasswords = []
 
-try:
-    pwWlFile: TextIOWrapper = open(args.users, "r")
-except FileNotFoundError:
-    error("Wordlist does not exist!")
+if args.users:
+    try:
+        usWlFile: TextIOWrapper = open(args.users, "r")
+    except FileNotFoundError:
+        error("Wordlist does not exist!")
+    else:
+        values = usWlFile.read()
+        usernames = values.split('\n')
+        usWlFile.close()
 else:
-    values = pwWlFile.read()
-    passwords = values.split('\n')
-    pwWlFile.close()
+    usernames = [args.user]
 
-for user in usernames:
+if args.passwords:
+    try:
+        pwWlFile: TextIOWrapper = open(args.passwords, "r")
+    except FileNotFoundError:
+        error("Wordlist does not exist!")
+    else:
+        values = pwWlFile.read()
+        passwords = values.split('\n')
+        pwWlFile.close()
+else:
+    passwords = [args.password]
+
+adjustedUsers = open('./adjusted-users.txt', 'w')
+adjustedPassword = open('./adjusted-passwords.txt', 'w')
+
+# Brute-force A User: Single user, with multiple passwords.
+if args.user and args.passwords:
+    for pwd in passwords:
+        adjustedUsers.write(args.validUser + '\n')
+        adjustedUsers.write(usernames[0] + '\n')
+
+        adjustedPassword.write(args.validPassword + '\n')
+        adjustedPassword.write(pwd + '\n')
+
+# Password Spray: Single password, with multiple users.
+if args.password and args.users:
+    for user in args.users:
+        adjustedUsers.write(args.validUser + '\n')
+        adjustedUsers.write(user + '\n')
+
+        adjustedPassword.write(args.validPassword + '\n')
+        adjustedPassword.write(passwords[0] + '\n')
+
+# Cluster Attack. Every account, can be tried against every password, with a valid credential in between each row.
+if args.passwords and args.users:
+    for user in args.users:
+        for pwd in args.password:
+            adjustedUsers.write(args.validUser + '\n')
+            adjustedUsers.write(user + '\n')
+
+            adjustedPassword.write(args.validPassword + '\n')
+            adjustedPassword.write(pwd + '\n')
